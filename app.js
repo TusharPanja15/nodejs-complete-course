9c8d6ac5b78e8e10e6566bb404a5dcd434bd9dca
Tushar Panja <panja.tushar15@gmail.com>
2022-06-04 18:09:30 +0530

    deployment-v1.1

--- app.js
+++ app.js
@@ -123,9 +123,6 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
-        // https
-        //     .createServer({ key: privateKey, cert: certificate }, app)
-        //     .listen(process.env.PORT || 3000);
         app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
